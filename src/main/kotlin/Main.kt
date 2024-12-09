0a096198648a4748fb31bcb7835ff4fba6a73655
mac <chuaimen@163.com>
2024-12-09 21:15:27 +0800

    第一次尝试提交

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,40 @@
+//TODO：GitHUb 应用
+//TODO：完成intelliJ 软件的学习课程
+//TODO: compose with Gradle应用教学
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+
+//TODO：ui框架 位置设置
+//TODO：跨文件 函数引用
+@Composable
+@Preview
+fun App() {
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    MaterialTheme {
+        Button(onClick = {
+            text = "Hello, Desktop!"
+        }) {
+            Text(text)
+            
+        }
+    }
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
+}
